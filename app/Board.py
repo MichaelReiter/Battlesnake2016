cba2dd8faaab3e21d4de192610aac8e065d643ba
MichaelReiter <michaelreiter@live.ca>
2016-02-20 13:01:26 -0800

    Update board creation

--- app/Board.py
+++ app/Board.py
@@ -1,12 +1,12 @@
 class Board:
-    def __init__(self, width, height, turn, number_of_snakes, food, walls, gold):
-        self.width = width
-        self.height = height
-        self.turn = turn
-        self.number_of_snakes = number_of_snakes
-        self.food = food
-        self.walls = walls
-        self.gold = gold
+    def __init__(self, data):
+        self.width = data.width
+        self.height = data.height
+        self.turn = data.turn
+        self.number_of_snakes = data.number_of_snakes
+        self.food = data.food
+        self.walls = data.walls
+        self.gold = data.gold
 
     def update(self, data):
         self.turn += 1
