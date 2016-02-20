1a44a4fdf19af518e052eba0041af46105a7162a
Erik Reppel <erikreppel@gmail.com>
2016-02-20 12:50:02 -0800

    Write closest_food util

--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,22 @@
-def find_food(snake, board):
+# def find_food(snake, board):
 
 
-def distance_to_food(board, snake):
-    x_distance to 
+def closest_food(board, snake):
+    if len(board.food) == 0:
+        return None
+
+    dist_to_food = 100000
+    closest_food = [0, 0]
+    for item in board.food:
+        x = snake.x - item[0]
+        y = snake.y - item[1]
+
+        sum_squared = x**2 + y**2
+        if sum_squared < dist_to_food:
+            dist_to_food = sum_squared
+            closest_food = [x, y]
+
+    x_dist = closest_food[0] - snake.x
+    y_dist = closest_food[1] - snake.y
+
+    return [x_dist, y_dist]
