0e33c0e5012f45116dbf446d67bd67d7350e3cbf
MichaelReiter <michaelreiter@live.ca>
2016-02-20 14:52:55 -0800

    Change data to dictonary syntax

--- app/board.py
+++ app/board.py
@@ -3,17 +3,17 @@ class Board:
         pass
 
     def start(self, data):
-        self.width = data.width
-        self.height = data.height
-        self.turn = data.turn
-        self.number_of_snakes = len(data.snakes)
-        self.food = data.food
-        self.walls = data.walls
-        self.gold = data.gold
+        self.width = data["width"]
+        self.height = data["height"]
+        self.turn = data["turn"]
+        self.number_of_snakes = len(data["snakes"])
+        self.food = data["food"]
+        self.walls = data["walls"]
+        self.gold = data["gold"]
 
     def update(self, data):
         self.turn += 1
-        self.number_of_snakes = len(data.snakes)
-        self.food = data.food
-        self.walls = data.walls
-        self.gold = data.gold
+        self.number_of_snakes = len(data["snakes"])
+        self.food = data["food"]
+        self.walls = data["walls"]
+        self.gold = data["gold"]
