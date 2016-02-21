0592bd6dc97adc18bb0feadfe1ce67480802c939
MichaelReiter <michaelreiter@live.ca>
2016-02-20 16:03:16 -0800

    Fix Erik's bug

--- app/snake.py
+++ app/snake.py
@@ -3,9 +3,9 @@ class Snake:
         pass
 
     def start(self, snake):
-        self.id = snake.id
-        self.coords = snake.coords
+        self.id = snake["id"]
+        self.coords = snake["coords"]
         self.x = self.coords[0][0]
         self.y = self.coords[0][1]
-        self.age = snake.age
-        self.health = snake.health
+        self.age = snake["age"]
+        self.health = snake["health"]
